b3b0cdfb3765b2e20a4a2a0e2d93345602b32f62
natthanicha6812032610134 <s6812032610134@email.kmutnb.ac.th>
2025-07-25 13:45:48 +0700

    Add files via upload

---
+++ coding-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  
+
+int main() {                      //int คือkeyword , main คือidentifier
+    printf("Hello,\n");           //printf คือidentifie , Hello,\n คือconstant
+    printf("C Tokens!\n");        //printf คือidentifie , c tokens!\n คือconstant
+    return 0;                     //return คือkeyword , 0 คือconstant
+}
